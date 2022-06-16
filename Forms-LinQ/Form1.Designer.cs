24b2992cab0ec409f824523e3dc18d659081090b
Roberto García Marcos <rgm43r@gorfactory.es>
2022-06-16 18:37:09 +0200

    Add project files.

---
+++ Forms-LinQ/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿
+namespace Forms_LinQ
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_orderScore = new System.Windows.Forms.Button();
+            this.btn_orderName = new System.Windows.Forms.Button();
+            this.Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Score = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Name,
+            this.Score});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(355, 433);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btn_orderName);
+            this.groupBox1.Controls.Add(this.btn_orderScore);
+            this.groupBox1.Location = new System.Drawing.Point(373, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(232, 228);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Actions";
+            // 
+            // btn_orderScore
+            // 
+            this.btn_orderScore.Location = new System.Drawing.Point(6, 22);
+            this.btn_orderScore.Name = "btn_orderScore";
+            this.btn_orderScore.Size = new System.Drawing.Size(150, 23);
+            this.btn_orderScore.TabIndex = 0;
+            this.btn_orderScore.Text = "Order by Score";
+            this.btn_orderScore.UseVisualStyleBackColor = true;
+            this.btn_orderScore.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // btn_orderName
+            // 
+            this.btn_orderName.Location = new System.Drawing.Point(6, 51);
+            this.btn_orderName.Name = "btn_orderName";
+            this.btn_orderName.Size = new System.Drawing.Size(150, 23);
+            this.btn_orderName.TabIndex = 1;
+            this.btn_orderName.Text = "Order by Name";
+            this.btn_orderName.UseVisualStyleBackColor = true;
+            // 
+            // Name
+            // 
+            this.Name.HeaderText = "Name";
+            this.Name.Name = "Name";
+            this.Name.ReadOnly = true;
+            this.Name.Width = 200;
+            // 
+            // Score
+            // 
+            this.Score.HeaderText = "Score";
+            this.Score.Name = "Score";
+            this.Score.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 457);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button btn_orderScore;
+        private System.Windows.Forms.Button btn_orderName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Score;
+    }
+}
+
---
+++ Testing-LinQ/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Testing_LinQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Lambdas and Where expressions
+            List<int> nums = new List<int> { 0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50 };
+            List<int> odds = nums.Where(x => x % 2 == 0).ToList();
+            Console.WriteLine("Odds numbers:");
+            odds.ForEach(odd => Console.WriteLine(odd));
+            Console.WriteLine("---------------------------\n\r");
+
+
+
+            // Select, First, Last, OrderBy, Average, Max, Min
+
+            List<Player> players = new List<Player>
+            {
+                new Player { Name = "Pedro", Score = 5 },
+                new Player { Name = "Jorge", Score = 8 },
+                new Player { Name = "Sandra", Score = 10 },
+                new Player { Name = "Pablo", Score = 4 },
+                new Player { Name = "Carol", Score = 6 },
+                new Player { Name = "Noe", Score = 7 },
+                new Player { Name = "Rober", Score = 13 },
+                new Player { Name = "Alberto", Score = 17 },
+                new Player { Name = "Pepe", Score = 1 },
+                new Player { Name = "Paquito", Score = 6 },
+                new Player { Name = "Andres", Score = 3 }
+            };
+
+            // Players order by name
+            Console.WriteLine("--== PLAYERS == --");
+            players.OrderBy(p => p.Name).ToList().ForEach(x => Console.WriteLine(x.ToString()));
+            
+            Console.WriteLine("--== SCORES ==--\n\r");
+            
+            // Higher score
+            Console.WriteLine($"Higher Score: {players.Max(x => x.ToString())}");
+        }
+    }
+
+    public class Player
+    {
+        public string Name { get; set; }
+        public int Score { get; set; }
+
+        public override string ToString()
+        {
+            return "{" + Name + " | " + Score + "}";
+        }
+    }
+}
